32b0eb27b82a01fdacd523c179fe3d78af424649
HipsterBrown <headhipster@hipsterbrown.com>
2020-04-12 20:54:56 +0100

    feat: use tera to build complete slide pages

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::{create_dir, read_to_string, remove_dir_all, File};
 use std::io::prelude::*;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use tera::{Context, Tera};
 
 /// md-slide CLI
 #[derive(StructOpt, Debug)]
@@ -16,6 +17,17 @@ struct Opt {
     #[structopt(name = "FILE", parse(from_os_str))]
     input: PathBuf,
 }
+
+static DEFAULT_SLIDE_TEMPLATE: &'static str = "<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <title>Slide {{ index }}</title>
+  </head>
+  <body>
+    {{ content }}
+  </body>
+</html>";
 
 fn main() -> std::io::Result<()> {
     let options = Opt::from_args();
@@ -31,13 +43,26 @@ fn main() -> std::io::Result<()> {
     // create destination directory for pages
     create_dir("./build")?;
 
+    let mut tera = Tera::default();
+    tera.autoescape_on(vec![]);
+    tera.add_raw_template("slide.html", DEFAULT_SLIDE_TEMPLATE)
+        .unwrap();
+
     for (index, page) in parsed_pages.enumerate() {
         let mut html_output = String::new();
         html::push_html(&mut html_output, page.to_vec().into_iter());
 
+        let mut context = Context::new();
+        context.insert("index", &index);
+        context.insert("content", &html_output);
+
+        let slide_html = tera.render("slide.html", &context).unwrap();
+
         let mut file = File::create(format!("./build/{}.html", index))?;
-        file.write_all(&html_output.bytes().collect::<Vec<u8>>())?;
+        file.write_all(&slide_html.bytes().collect::<Vec<u8>>())?;
     }
+
+    println!("Slides created!");
 
     Ok(())
 }
